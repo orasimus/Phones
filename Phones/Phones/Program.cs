353c66a0eff13d75006aa51d4fd14490d1df87ff
Otso Rasimus <orasimus@gmail.com>
2014-06-01 22:14:15 +0300

    Add program.

---
+++ Phones/Phones/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Phones
+{
+    class Program
+    {
+        private static string template = @"BEGIN:VCARD" + Environment.NewLine +
+                                          "VERSION:3.0" + Environment.NewLine +
+                                          "FN:{0}" + Environment.NewLine +
+                                          "N:;{0};;;" + Environment.NewLine +
+                                          "TEL;TYPE=CELL:{1}" + Environment.NewLine +
+                                          "END:VCARD";
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 2 && File.Exists(args[0]))
+            {
+                throw new ArgumentException("Invalid args");
+            }
+
+            var contacts = File.ReadAllLines(args[0]);
+            var newContacts = contacts.Select(Transform);
+            File.WriteAllLines(args[1], newContacts);
+        }
+
+        private static string Transform(string contact)
+        {
+            var split = contact.Split(',');
+            var name = split[0];
+            var number = split[1];
+            return string.Format(template, name, number);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Phone contact information transformer
+
+Transforms contact information from a vcf format to vcard format. The latter is accepted by Google Contacts import.
+
+I wrote this program because I had to figure a way to export Lumia 900 (Windows Phone 7.8) contacts to Google Contacts. The source file was created with Contacts Backup by John Li: http://www.windowsphone.com/fi-fi/store/app/contacts-backup/7cc19164-76dc-4f13-93d3-a368fa864d56.
+
+If your contact names have commas in them, you need to edit those out manually before using the program.
+
+Source format example:
+
+    Jacob,04011111
+    Steve,+241000000
